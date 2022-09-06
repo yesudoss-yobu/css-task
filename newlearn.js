cce8867cfecc1c87254acf4afdaeaba95c5a117a
yesudoss-yobu <stalindoss18@gmail.com>
2022-09-06 12:05:06 +0530

    first commit

---
+++ newlearn.js
@@ -0,0 +1,9 @@
+function mymodal(){
+
+if (document.getElementById("modal").style.display=="none") {
+    
+    document.getElementById("modal").style.display="block"
+} else {
+    document.getElementById("modal").style.display="none"
+}
+}
